93ccecf6a27a7e8e80eecfe9ff46a079d7a63684
oleshche <oleshche@student.unit.ua>
2018-03-15 18:52:18 +0200

    one more try

--- main.c
+++ main.c
@@ -16,10 +16,24 @@
 
 int		main(void)
 {
-    //setlocale(LC_ALL, "");
-    int value = 65;
-    int ret1 = ft_printf("%C", (wchar_t)value);
-    int ret2 = printf("%C", (wchar_t)value);
-    ft_printf("\nmy: ret = %d\toriginal: ret = %d\n", ret1, ret2);
+	setlocale(LC_ALL, "");
+	/*int value = 224;
+	//int i = 0;
+	int ret1;
+	int ret2;
+	*//*while (i < 255)
+	{
+		ret1 = ft_printf("%c\n", i);
+		ret2 = printf("%c\n", i);
+		if (ret1 != ret2)
+			printf("value = %d\tret1 = %d\tret2 = %d\n", i, ret1, ret2);
+		i++;
+	}*//*
+	ret1 = ft_printf("%ls", L"42Kashim a 1001 histoires à raconterIl fait au moins");
+	ret2 = printf("%ls", L"42Kashim a 1001 histoires à raconterIl fait au moins");
+	ft_printf("\nret1 = %d\tret2 = %d\n", ret1, ret2);
+	*/
+    ft_printf("%lc\n", L'à');
+    printf("%lc", L'à');
     return (0);
 }
