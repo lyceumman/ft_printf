1391220c859c70e3527c17835305ac080a74de18
oleshche <oleshche@student.unit.ua>
2018-03-20 16:34:55 +0200

    try our put_str

--- libft/ft_putstr.c
+++ libft/ft_putstr.c
@@ -20,5 +20,13 @@ void	ft_putstr(char const *s)
 		return ;
 	str = (char*)s;
 	while (*str)
-		write(1, str++, 1);
+	{
+		if((*str) < 0)
+		{
+			write(1, str++, 2);
+			str++;
+		}
+		else
+			write(1, str++, 1);
+	}
 }
--- main.c
+++ main.c
@@ -15,8 +15,20 @@
 
 int		main(void)
 {
-    printf("%s\n", setlocale(LC_ALL, "Fr_CH"));
-    int ret1 = ft_printf("|%+.0o|", 0);
-    int ret2 = printf("|%+.0o|", 0);
+//    printf("%s\n", setlocale(LC_ALL, "Fr_CH"));
+//    int ret1 = ft_printf("|%+.0o|", 0);
+//    int ret2 = printf("|%+.0o|", 0);
+//    ft_printf("\nmy: ret = %d\torg: ret = %d\n", ret2 ,ret2);
+
+    printf("%s\n", setlocale(LC_CTYPE, "Fr_CH"));
+    char *string = "42Kashim a 1001 histoires à raconterIl fait au moins -8000";
+    int a = '\xc3';
+    int ret1 = ft_printf("%s", string);
+    int ret2 = printf("%s", string);
     ft_printf("\nmy: ret = %d\torg: ret = %d\n", ret2 ,ret2);
+    ft_printf("a = %d\n", a);
+    ft_printf("a = %d\n", (char)a);
+    ft_printf("a = %d\n", (unsigned char)a);
+
+    return (0);
 }
